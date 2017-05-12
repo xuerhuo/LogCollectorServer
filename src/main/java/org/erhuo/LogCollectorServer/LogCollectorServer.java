fbc039daf7e44d240f64406cb2c4f2db77f82fbb
xuerhuo <824851582@qq.com>
2017-05-12 17:55:55 +0800

    elastic

--- src/main/java/org/erhuo/LogCollectorServer/LogCollectorServer.java
+++ src/main/java/org/erhuo/LogCollectorServer/LogCollectorServer.java
@@ -9,29 +9,36 @@ import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import static java.lang.Thread.sleep;
+import java.io.File;
+import java.util.LinkedList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by erhuo on 2017/5/9.
  */
-public class LogCollectorServer {
+public class LogCollectorServer{
     public static HashMap<String, String> margs;
     public static HashMap<String, String> parselog;
     public static NginxLogParse ng=new NginxLogParse();
     public static Redis redis;
     public static void main(String[] args) throws InterruptedException, UnknownHostException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(50);
         String temp;
+        long len;
         margs = Tools.initargs(args);
         initSys();
-        System.exit(1);
         System.out.print(redis.llen(margs.get("logrediskey")));
         while (true) {
-            while (redis.llen(margs.get("logrediskey")) > 1) {
+            len=redis.llen(margs.get("logrediskey"));
+            System.out.println(len);
+            while ( len> 1) {
                 temp = redis.lpop(margs.get("logrediskey"));
                 temp =Base64.decode(temp);
                     parselog = ng.parse(temp);
-
-                Thread.sleep(2000);
-                OutPut.output(Tools.mapToStringOrder(parselog,"|"));
+                threadPool.execute(new SingleThread(parselog));
+                len=redis.llen(margs.get("logrediskey"));
+                System.out.println(len);
             }
             try {
                 Thread.sleep(1000);
@@ -50,8 +57,5 @@ public class LogCollectorServer {
         System.out.println(margs.get("els_host")+margs.get("els_port"));
         OutPut.client=new PreBuiltTransportClient(Settings.EMPTY)
                 .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(margs.get("els_host")), Integer.parseInt(margs.get("els_port"))));
-        System.out.print(OutPut.client.nodeName());
     }
-
-
 }
--- src/main/java/org/erhuo/LogCollectorServer/OutPut.java
+++ src/main/java/org/erhuo/LogCollectorServer/OutPut.java
@@ -1,25 +1,42 @@
 package org.erhuo.LogCollectorServer;
 
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.*;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.rest.RestStatus;
 
+import javax.print.DocFlavor;
 import java.net.InetAddress;
 import java.net.InterfaceAddress;
 import java.net.UnknownHostException;
+import java.util.HashMap;
 
 /**
  * Created by erhuo on 2017/5/9.
  */
 public class OutPut {
     public static TransportClient client;
-    public static void output(String vale) {
+    public static void output(HashMap<String,String> vale) {
         if(LogCollectorServer.margs.get("outputype").equals("elasticsearch")){
-           elsoutput(vale);
+          elsoutput(vale);
         }
     }
-    public static void elsoutput(String value) {
-        System.out.println(value);
+    public static int elsoutput(HashMap<String,String> json) {
+        IndexResponse response = client.prepareIndex("twitter", "tweet")
+                .setSource(json)
+                .get();
+        // Index name
+        String _index = response.getIndex();
+// Type name
+        String _type = response.getType();
+// Document ID (generated or not)
+        String _id = response.getId();
+// Version (if it's the first time you index this document, you will get: 1)
+        long _version = response.getVersion();
+// status has stored current instance statement.
+        RestStatus status = response.status();
+        return status.getStatus();
     }
 }
